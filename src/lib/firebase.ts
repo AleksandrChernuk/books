b43cf63e22a9bd2da1926e02b8e2ab39c6f6d115
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-17 17:57:03 +0300

    remove nested

---
+++ src/lib/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
+import { getFirestore, Timestamp } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+
+export const functions = getFunctions(app);
+export type TTimestamp = Timestamp;
